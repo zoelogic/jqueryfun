fc0a97f8f2d0c0655e94fc53dff8306da6a0a57f
Jon Swenson <js@zoelogic.com>
2012-05-05 23:46:37 +0700

    Added basic structure for jon.php script.
    /images 		-- made two buttons w. rollover image too.
    /js				-- made jon.js main javascript page for jon.php
    	/helpers	-- made a helpers folder for javascript scripts.
    				   Added two files to the folder:

--- jon.php
+++ jon.php
@@ -1,7 +1,37 @@
-<?php
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-<html>
+<head>
+	<title>An XHTML 1.0 Strict standard template</title>
+	<meta http-equiv="content-type" 
+		content="text/html;charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="layout.css" />
+		<script type="text/javascript" src="js/jquery172.js"></script>
+		<script type="text/javascript" src="js/jon.js"></script>
+</head>
+
+<body>
+
 <h1>Jon's jQuery Hacks :-)</h1>
+<div>
 <a href="index.php">jQuery Project Index</a>
+</div>
+  
+  
+<div id="jonPageNavigator">
+	<div class="button1">
+		<a class="targetButton1" href="" title="button1"> </a>
+	</div>
+	<div class="button2">
+		<a class="targetButton2" href="" title="button2"> </a>
+	</div>
+
+
+</div>  
+   
+
+</body>
 </html>
+
+
